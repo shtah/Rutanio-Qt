84d072034f961dab0f309e96b1cd0aaa86ef36b1
turcol <turcol@gmail.com>
2019-08-02 09:28:37 -0400

    Update Chain Main parameters

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -71,13 +71,13 @@ public:
         // The message start string is designed to be unlikely to occur in normal data.
         // The characters are rarely used upper ASCII, not valid as UTF-8, and produce
         // a large 4-byte int at any alignment.
-        pchMessageStart[0] = 0x28;
-        pchMessageStart[1] = 0x62;
-        pchMessageStart[2] = 0x48;
-        pchMessageStart[3] = 0x76;
-        vAlertPubKey = ParseHex("0486bce1bac0d543f104cbff2bd23680056a3b9ea05e1137d2ff90eeb5e08472eb500322593a2cb06fbf8297d7beb6cd30cb90f98153b5b7cce1493749e41e0284");
-        nDefaultPort = 4562;
-        nRPCPort = 4561;
+        pchMessageStart[0] = 0x26;
+        pchMessageStart[1] = 0x75;
+        pchMessageStart[2] = 0x52;
+        pchMessageStart[3] = 0x09;
+        vAlertPubKey = ParseHex("04cac64149c07f3daa817f56f7d29aba28098e64ee01749f935613d586b43041582816236d31f26c0ec1921688e7202c0fa7a157ad555763896b890d8daac02f39");
+        nDefaultPort = 6782;
+        nRPCPort = 6781;
         bnProofOfWorkLimit = CBigNum(~uint256(0) >> 20);
 
         // Build the genesis block. Note that the output of the genesis coinbase cannot
@@ -88,8 +88,8 @@ public:
         //    CTxIn(COutPoint(0000000000, 4294967295), coinbase 00012a24323020466562203230313420426974636f696e2041544d7320636f6d6520746f20555341)
         //    CTxOut(empty)
         //  vMerkleTree: 12630d16a9
-        const char* pszTimestamp = "http://www.bbc.com/news/world-middle-east-43691291";
-        int64_t genesisTime = 1523205120;                // 2018-04-08 16:32:00 UTC
+        const char* pszTimestamp = "https://edition.cnn.com/2019/07/28/sport/tour-de-france-bernal-colombia-spt-intl/index.html";
+        int64_t genesisTime = 1564350120;                // 2019-07-28 21:42:00 UTC
         std::vector<CTxIn> vin;
         vin.resize(1);
         vin[0].scriptSig = CScript() << 0 << CBigNum(42) << vector<unsigned char>((const unsigned char*)pszTimestamp, (const unsigned char*)pszTimestamp + strlen(pszTimestamp));
@@ -103,21 +103,21 @@ public:
         genesis.nVersion = 1;
         genesis.nTime    = genesisTime;
         genesis.nBits    = bnProofOfWorkLimit.GetCompact();
-        genesis.nNonce   = 842767;
+        genesis.nNonce   = 288859;
 
         hashGenesisBlock = genesis.GetHash();
 
-        assert(hashGenesisBlock == uint256("0x00000036090a68c523471da7a4f0f958c1b4403fef74a003be7f71877699cab7"));
-        assert(genesis.hashMerkleRoot == uint256("0x85c4a8a116eb457ff74bb64908e71c6780bff7e69ad3dadc9df6cd753c21f937"));
+        assert(hashGenesisBlock == uint256("0x00000347c656a618f9bfef80a14fa66cf26e34ed4caeba0e3f072eb8b9408ee6"));
+        assert(genesis.hashMerkleRoot == uint256("0xa74f9cb5ad97977b1e1079658f8290aa1e6122ee50df327a7e39480f94237c54"));
 
-        vSeeds.push_back(CDNSSeedData("Seednode1", "seednode1.oexo.cloud"));
-        vSeeds.push_back(CDNSSeedData("Seednode2", "seednode2.oexo.net"));
-        vSeeds.push_back(CDNSSeedData("Seednode3", "seednode3.oexo.cloud"));
-        vSeeds.push_back(CDNSSeedData("Seednode4", "seednode4.oexo.net"));
+        vSeeds.push_back(CDNSSeedData("Seednode1", "seednode1.rutan.cloud"));
+        vSeeds.push_back(CDNSSeedData("Seednode2", "seednode2.rutan.network"));
+        vSeeds.push_back(CDNSSeedData("Seednode3", "seednode3.rutan.cloud"));
+        vSeeds.push_back(CDNSSeedData("Seednode4", "seednode4.rutan.network"));
 
-        base58Prefixes[PUBKEY_ADDRESS] = std::vector<unsigned char>(1, 28);
-        base58Prefixes[SCRIPT_ADDRESS] = std::vector<unsigned char>(1, 87);
-        base58Prefixes[SECRET_KEY] =     std::vector<unsigned char>(1, (28+128));
+        base58Prefixes[PUBKEY_ADDRESS] = std::vector<unsigned char>(1, 60);
+        base58Prefixes[SCRIPT_ADDRESS] = std::vector<unsigned char>(1, 122);
+        base58Prefixes[SECRET_KEY] =     std::vector<unsigned char>(1, (60+128));
         base58Prefixes[EXT_PUBLIC_KEY] = boost::assign::list_of(0x04)(0x88)(0xB2)(0x1E).convert_to_container<std::vector<unsigned char> >();
         base58Prefixes[EXT_SECRET_KEY] = boost::assign::list_of(0x04)(0x88)(0xAD)(0xE4).convert_to_container<std::vector<unsigned char> >();
 
@@ -149,39 +149,37 @@ public:
         // The message start string is designed to be unlikely to occur in normal data.
         // The characters are rarely used upper ASCII, not valid as UTF-8, and produce
         // a large 4-byte int at any alignment.
-        pchMessageStart[0] = 0x25;
-        pchMessageStart[1] = 0x16;
-        pchMessageStart[2] = 0x74;
-        pchMessageStart[3] = 0x62;
+        pchMessageStart[0] = 0x41;
+        pchMessageStart[1] = 0x28;
+        pchMessageStart[2] = 0x55;
+        pchMessageStart[3] = 0x19;
         bnProofOfWorkLimit = CBigNum(~uint256(0) >> 16);
         vAlertPubKey = ParseHex("0471dc165db490094d35cde15b1f5d755fa6ad6f2b5ed0f340e3f17f57389c3c2af113a8cbcc885bde73305a553b5640c83021128008ddf882e856336269080496");
-        nDefaultPort = 14562;
-        nRPCPort = 14561;
+        nDefaultPort = 16782;
+        nRPCPort = 16781;
 
         strDataDir = "testnet";
         // Modify the testnet genesis block so the timestamp is valid for a later start.
         genesis.nBits  = bnProofOfWorkLimit.GetCompact();
-        genesis.nNonce = 440504;
-        genesis.nTime  = 1528761600;                  // 2018-06-12 00:00:00 UTC
+        genesis.nNonce = 5218499;
+        genesis.nTime  = 1542885720;
 
         hashGenesisBlock = genesis.GetHash();
 
-        assert(hashGenesisBlock == uint256("0x0000059bb2c2048493efcb0f1a034972b3ce4089d54c93b69aaab212fb369887"));
+        assert(hashGenesisBlock == uint256("0x000000a45fb1d49065d6bad6ca84e5130dc319e0db5003d14a6c201a5b2895a0"));
 
         vFixedSeeds.clear();
         vSeeds.clear();
-        vSeeds.push_back(CDNSSeedData("testseednode1", "testseednode1.oexo.cloud"));
-        vSeeds.push_back(CDNSSeedData("testseednode2", "testseednode2.oexo.net"));
-        vSeeds.push_back(CDNSSeedData("testseednode3", "testseednode3.oexo.cloud"));
-        vSeeds.push_back(CDNSSeedData("testseednode4", "testseednode4.oexo.net"));
+        vSeeds.push_back(CDNSSeedData("testseednode1.rutan.cloud", "testseednode2.rutan.network"));
 
-        base58Prefixes[PUBKEY_ADDRESS] = std::vector<unsigned char>(1, 75);    // exos test net start with X
-        base58Prefixes[SCRIPT_ADDRESS] = std::vector<unsigned char>(1, 206);
-        base58Prefixes[SECRET_KEY]     = std::vector<unsigned char>(1, 75 + 128);
+        base58Prefixes[PUBKEY_ADDRESS] = std::vector<unsigned char>(1, 68); // rutanio test net
+        base58Prefixes[SCRIPT_ADDRESS] = std::vector<unsigned char>(1, 199);
+        base58Prefixes[SECRET_KEY]     = std::vector<unsigned char>(1, 68 + 128);
         base58Prefixes[EXT_PUBLIC_KEY] = boost::assign::list_of(0x04)(0x35)(0x87)(0xCF).convert_to_container<std::vector<unsigned char> >();
         base58Prefixes[EXT_SECRET_KEY] = boost::assign::list_of(0x04)(0x35)(0x83)(0x94).convert_to_container<std::vector<unsigned char> >();
 
         convertSeed6(vFixedSeeds, pnSeed6_test, ARRAYLEN(pnSeed6_test));
+
 
         nLastPOWBlock = 0x7fffffff;
     }
@@ -196,19 +194,19 @@ static CTestNetParams testNetParams;
 class CRegTestParams : public CTestNetParams {
 public:
     CRegTestParams() {
-        pchMessageStart[0] = 0x4a;
-        pchMessageStart[1] = 0xdb;
-        pchMessageStart[2] = 0xea;
-        pchMessageStart[3] = 0xfc;
+        pchMessageStart[0] = 0xfa;
+        pchMessageStart[1] = 0xbf;
+        pchMessageStart[2] = 0xb5;
+        pchMessageStart[3] = 0xda;
         bnProofOfWorkLimit = CBigNum(~uint256(0) >> 1);
-        genesis.nTime  = 1391501792;
+        genesis.nTime = 1411111111;
         genesis.nBits  = bnProofOfWorkLimit.GetCompact();
-        genesis.nNonce = 4249953;
+        genesis.nNonce = 232431;
         hashGenesisBlock = genesis.GetHash();
-        nDefaultPort = 15888;
+        nDefaultPort = 18999;
         strDataDir = "regtest";
 //        MineGenesis(genesis);
-        assert(hashGenesisBlock == uint256("0x00000755a53922ad3443a7609ee700ca246a547783116f2085fff1e486e56085"));
+        assert(hashGenesisBlock == uint256("0x000001eaf5fc73c5a2ded387a256d79b34b0acaaead5767a52c8c6081b79d031"));
 
         vSeeds.clear();  // Regtest mode doesn't have any DNS seeds.
     }
